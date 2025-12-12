c47b5df426c873f56de99162a5873589a231c538
rapon1kt <shinnatensei@gmail.com>
2025-12-12 18:54:18 -0300

    feat: implementing login and logout logic

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,55 @@
 import net from "net";
 import { Log, Send } from "@/actions";
+import { prisma } from "./config/prisma/db";
+import send from "./actions/send";
 require("dotenv").config();
 
 const server = net.createServer();
+const sessions = new Map<net.Socket, number>();
 
 server.on("connection", (socket) => {
   const clientId = `${socket.remoteAddress}:${socket.remotePort}`;
   console.log(`[${clientId}] connected`);
 
-  socket.on("data", (buffer) => {
+  socket.on("data", async (buffer) => {
     const message = buffer.toString().trim();
-    Log(socket, `Message received: ${message}`);
-    Send(socket, `Server received: ${message}`);
+    const [command, ...args] = message.split(" ");
+    if (command === "LOGIN") {
+      const username = args[0];
+      if (!username) return Send(socket, "ERRO: please insert an username.");
+      let user = await prisma.user.findUnique({
+        where: { username },
+      });
+      if (!user) {
+        user = await prisma.user.create({
+          data: { username },
+        });
+        send(socket, `USER created: ${username}`);
+      } else {
+        send(socket, `Welcome back, ${username}!`);
+      }
+      sessions.set(socket, user.id);
+      Log(socket, `USER connected with id: ${user.id}`);
+      return;
+    }
+
+    const userId = sessions.get(socket);
+    if (!userId) return send(socket, `ERRO: login is necessary.`);
+
+    if (command === "LOGOUT") {
+      sessions.delete(socket);
+      send(socket, `LOGOUT with success.`);
+      return;
+    }
+    send(socket, "Command not found.");
   });
 
   socket.on("end", () => {
-    console.log(`[${clientId}] disconnected`);
+    const userId = sessions.get(socket);
+    if (userId) {
+      console.log(`[${clientId}] disconnected, removing session...`);
+      sessions.delete(socket);
+    }
   });
 
   socket.on("error", (err) => {
