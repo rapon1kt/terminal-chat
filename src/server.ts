f49d5b71c3d6119141db6d54b5a4e7b524c1a3aa
rapon1kt <shinnatensei@gmail.com>
2025-12-12 18:09:47 -0300

    feat: implementing new actions in server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
 import net from "net";
+import { Log, Send } from "@/actions";
 require("dotenv").config();
 
 const server = net.createServer();
 
-server.on("connection", (client) => {
-  console.log("Client just connected :)");
-  client.write("Hello from server!");
-  client.on("end", () => {
-    console.log(`Client disconnected :(`);
+server.on("connection", (socket) => {
+  const clientId = `${socket.remoteAddress}:${socket.remotePort}`;
+  console.log(`[${clientId}] connected`);
+
+  socket.on("data", (buffer) => {
+    const message = buffer.toString().trim();
+    Log(socket, `Message received: ${message}`);
+    Send(socket, `Server received: ${message}`);
+  });
+
+  socket.on("end", () => {
+    console.log(`[${clientId}] disconnected`);
+  });
+
+  socket.on("error", (err) => {
+    console.log(`[${clientId}] ERROR: ${err}`);
   });
 });
 
